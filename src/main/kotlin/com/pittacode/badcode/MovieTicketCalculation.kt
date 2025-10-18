ef611557cda97462ceffdc75b96d14410d9cf448
Andrea Andreou <andreas.andreou@masabi.com>
2025-10-18 17:00:23 +0300

    Rename method name for clarity. Rename variables to better reflect what they are. Change formatting for better readability.

--- src/main/kotlin/com/pittacode/badcode/MovieTicketCalculation.kt
+++ src/main/kotlin/com/pittacode/badcode/MovieTicketCalculation.kt
@@ -3,37 +3,30 @@ package com.pittacode.badcode
 import java.time.DayOfWeek
 import kotlin.math.ceil
 
-fun calculate(request: MovieTicketRequest): Double {
-    var ticketPrice = 40.0
+fun calculateTicketPrice(request: MovieTicketRequest): Double {
+    var basePrice = 40.0
 
-    if (request.date.dayOfWeek == DayOfWeek.SATURDAY
-        || request.date.dayOfWeek == DayOfWeek.SUNDAY
-    ) {
-        ticketPrice = 50.0
+    if (request.date.dayOfWeek == DayOfWeek.SATURDAY || request.date.dayOfWeek == DayOfWeek.SUNDAY) {
+        basePrice = 50.0
     }
 
     var reduction = 0
-    if (request.date.dayOfWeek == DayOfWeek.TUESDAY
-        || request.date.dayOfWeek == DayOfWeek.WEDNESDAY
-    ) {
+    if (request.date.dayOfWeek == DayOfWeek.TUESDAY || request.date.dayOfWeek == DayOfWeek.WEDNESDAY) {
         reduction = 25
     }
 
     if (request.age < 10) {
-        if (request.date.dayOfWeek == DayOfWeek.SATURDAY
-            || request.date.dayOfWeek == DayOfWeek.SUNDAY
-        ) {
-            ticketPrice /= 2
+        if (request.date.dayOfWeek == DayOfWeek.SATURDAY || request.date.dayOfWeek == DayOfWeek.SUNDAY) {
+            basePrice /= 2
         }
 
-        val finalPriceChildren = ticketPrice * (1 - reduction / 100.0)
-        return ceil(ticketPrice)
+        val finalPriceChildren = basePrice * (1 - reduction / 100.0)
+        return ceil(basePrice)
     }
 
-    val finalPrice = ticketPrice * (1 - reduction / 100.0)
+    val finalPrice = basePrice * (1 - reduction / 100.0)
     return ceil(finalPrice)
 }
-
 
 data class MovieTicketRequest(val age: Int, val date: Date)
 data class Date(val dayOfWeek: DayOfWeek)
